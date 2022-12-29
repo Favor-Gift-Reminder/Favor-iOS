2ac14d8c237b36ba0c409b4f3fe9c8e5da530fa9
이창준 <crystalashe@naver.com>
2022-12-29 16:52:54 +0900

    [Init] 프로젝트 시작 🚀

---
+++ Favor/Favor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Favor
+//
+//  Created by 이창준 on 2022/12/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
