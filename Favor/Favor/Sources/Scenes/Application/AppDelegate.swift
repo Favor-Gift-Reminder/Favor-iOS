4733f7a58a40350bca9aa812eb3765b8fc1305ff
이창준 <crystalashe@naver.com>
2022-12-30 16:04:35 +0900

    [Chore] Indentation Lint 경고 해결

--- Favor/Favor/Sources/Scenes/Application/AppDelegate.swift
+++ Favor/Favor/Sources/Scenes/Application/AppDelegate.swift
@@ -11,16 +11,17 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-			// Override point for customization after application launch.
-			return true
-    }
+		// Override point for customization after application launch.
+		return true
+		
+	}
 
 	// MARK: UISceneSession Lifecycle
 
 	func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-			// Called when a new scene session is being created.
-			// Use this method to select a configuration to create the new scene with.
-			return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+		// Called when a new scene session is being created.
+		// Use this method to select a configuration to create the new scene with.
+		return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
 	}
 
 	func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
