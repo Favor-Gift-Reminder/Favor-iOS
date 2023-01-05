668a0bfca6f7825a006c8e59b20e293ad8cd6fe8
이창준 <crystalashe@naver.com>
2023-01-06 01:15:37 +0900

    [Feat] 브랜드 색상 상수 파일 추가

---
+++ Favor/Favor/Configuration/Constants/FavorStyle.swift
@@ -0,0 +1,42 @@
+//
+//  FavorStyle.swift
+//  Favor
+//
+//  Created by 이창준 on 2023/01/06.
+//
+
+import UIKit
+
+enum FavorStyle {
+  
+  enum Color {
+    case white, black
+    case main, sub, error
+    case background, box1, box2, detail, typo
+    
+    var value: UIColor {
+      switch self {
+      case .white:
+        return UIColor("#FFFFFF")
+      case .black:
+        return UIColor("#000000")
+      case .main:
+        return UIColor("#FF5862")
+      case .sub:
+        return UIColor("#0C1338")
+      case .error:
+        return UIColor("#2DB399")
+      case .background:
+        return UIColor("#F5F5F5")
+      case .box1:
+        return UIColor("#E0E0E0")
+      case .box2:
+        return UIColor("#BDBDBD")
+      case .detail:
+        return UIColor("#9E9E9E")
+      case .typo:
+        return UIColor("#424242")
+      }
+    }
+  }
+}
