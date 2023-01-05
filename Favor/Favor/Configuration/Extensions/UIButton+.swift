004118236bf38f1781728f2468d294760b62d242
이창준 <crystalashe@naver.com>
2023-01-06 01:45:15 +0900

    [FEAT] Configuration 이용하여 버튼 생성하는 extension 추가

---
+++ Favor/Favor/Configuration/Extensions/UIButton+.swift
@@ -0,0 +1,54 @@
+//
+//  UIButon+.swift
+//  Favor
+//
+//  Created by 이창준 on 2023/01/06.
+//
+
+import UIKit
+
+// MARK: - UIButton
+
+extension UIButton {
+  
+}
+
+// MARK: - UIButton Configuraiton
+
+@available(iOS 15.0, *)
+extension UIButton.Configuration {
+  
+  enum FavorButton {
+    case large, regular, small
+    
+    /// 버튼의 배경 색상
+    var backgroundColor: UIColor? {
+      switch self {
+      default:
+        return FavorStyle.Color.typo.value
+      }
+    }
+    
+    /// 버튼의 내용 색상
+    var foregroundColor: UIColor? {
+      switch self {
+      default:
+        return FavorStyle.Color.white.value
+      }
+    }
+  }
+  
+  /// Pre-configured된 UIButton.Configuration에 따라 UIButton을 생성합니다.
+  /// - Parameters:
+  ///     - style: 버튼의 스타일 (large, regular, small)
+  ///     - title: 버튼의 텍스트
+  static func makeButton(with style: FavorButton, title: String? = nil) -> UIButton.Configuration {
+    var configuration = UIButton.Configuration.filled()
+    configuration.title = title
+    configuration.titleAlignment = .center
+    configuration.background.cornerRadius = 12.0 // TODO: - 정확한 값 여쭤보기.
+    configuration.baseBackgroundColor = style.backgroundColor
+    configuration.baseForegroundColor = style.foregroundColor
+    return configuration
+  }
+}
