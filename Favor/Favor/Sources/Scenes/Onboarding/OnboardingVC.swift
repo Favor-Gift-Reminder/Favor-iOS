17c19407b23eda6a3511428d9e102b98ab3fbb08
eung7 <wannabeeung7@gmail.com>
2023-01-15 19:14:00 +0900

    [Feat] OnboardingVC CollectionView 및 PageControl 관련 비즈니스 로직 설정

--- Favor/Favor/Sources/Scenes/Onboarding/OnboardingVC.swift
+++ Favor/Favor/Sources/Scenes/Onboarding/OnboardingVC.swift
@@ -23,15 +23,44 @@ final class OnboardingViewController: BaseViewController {
   }()
   
   private lazy var collectionView: UICollectionView = {
-    let layout = UICollectionViewFlowLayout()
-    let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+    let cv = UICollectionView(frame: .zero, collectionViewLayout: onboardingLayout())
+    cv.register(
+      OnboardingCell.self,
+      forCellWithReuseIdentifier: OnboardingCell.identifier
+    )
+    cv.isScrollEnabled = false
+    cv.showsHorizontalScrollIndicator = false
+    cv.dataSource = self
+    cv.delegate = self
     
     return cv
   }()
   
+  private lazy var onboardingSection: NSCollectionLayoutSection = {
+    let size = NSCollectionLayoutSize(
+      widthDimension: .absolute(view.frame.width),
+      heightDimension: .absolute(collectionView.frame.height)
+    )
+
+    let item = NSCollectionLayoutItem(layoutSize: size)
+    let group = NSCollectionLayoutGroup.horizontal(layoutSize: size, subitems: [item])
+    let section = NSCollectionLayoutSection(group: group)
+    section.orthogonalScrollingBehavior = .paging
+
+    // PageControl currentPage 반응 Closure
+    section.visibleItemsInvalidationHandler = { _, offset, _ in
+      let width = self.collectionView.frame.width
+      self.pageControl.currentPage = Int(offset.x / width)
+    }
+    
+    return section
+  }()
+  
   // MARK: - Setup
   
-  override func setupStyles() {}
+  override func setupStyles() {
+    view.backgroundColor = .white
+  }
   
   override func setupLayouts() {
     [
@@ -45,7 +74,7 @@ final class OnboardingViewController: BaseViewController {
   override func setupConstraints() {
     pageControl.snp.makeConstraints { make in
       make.centerX.equalToSuperview()
-      make.top.equalToSuperview().inset(66)
+      make.top.equalTo(view.safeAreaLayoutGuide).inset(66)
     }
     
     collectionView.snp.makeConstraints { make in
@@ -55,11 +84,37 @@ final class OnboardingViewController: BaseViewController {
   }
 }
 
-#if canImport(SwiftUI) && DEBUG
-import SwiftUI
-struct OnboardingVC_PreView: PreviewProvider {
-  static var previews: some View {
-    OnboardingViewController().toPreview()
+// MARK: - Setup CollectionView
+
+extension OnboardingViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+  func collectionView(
+    _ collectionView: UICollectionView,
+    cellForItemAt indexPath: IndexPath
+  ) -> UICollectionViewCell {
+    guard let cell = collectionView.dequeueReusableCell(
+      withReuseIdentifier: OnboardingCell.identifier,
+      for: indexPath
+    ) as? OnboardingCell else {
+      return UICollectionViewCell()
+    }
+
+    if indexPath.row == 2 {
+      cell.startBtn.isHidden = false
+    }
+    
+    return cell
+  }
+  
+  func collectionView(
+    _ collectionView: UICollectionView,
+    numberOfItemsInSection section: Int
+  ) -> Int {
+    return 3
+  }
+  
+  private func onboardingLayout() -> UICollectionViewCompositionalLayout {
+    return UICollectionViewCompositionalLayout { [weak self] _, _ in
+      return self?.onboardingSection
+    }
   }
 }
-#endif
