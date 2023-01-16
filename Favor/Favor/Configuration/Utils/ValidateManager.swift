7bfd28ebda56e724ca5d51814cc567fb2d38c411
이창준 <crystalashe@naver.com>
2023-01-16 21:44:50 +0900

    [FEAT] 이메일/비밀번호 정규식, validate 메서드 추가

--- Favor/Favor/Configuration/Utils/ValidateManager.swift
+++ Favor/Favor/Configuration/Utils/ValidateManager.swift
@@ -9,10 +9,41 @@ import Foundation
 
 enum ValidateManager {
   
-  enum Email {
+  enum EmailValidate {
     case empty
     case invalid
     case valid
+    
+    static let regex: String = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+    
+    var description: String? {
+      switch self {
+      case .empty:
+        return "이메일을 입력해주세요."
+      case .invalid:
+        return "올바르지 않은 이메일 형식입니다."
+      case .valid:
+        return nil
+      }
+    }
+  }
+  
+  static func validate(email: String) -> EmailValidate {
+    guard !email.isEmpty else {
+      return .empty
+    }
+    guard email.range(of: EmailValidate.regex, options: .regularExpression) != nil else {
+      return .invalid
+    }
+    return .valid
+  }
+  
+  enum PasswordValidate {
+    case empty
+    case invalid
+    case valid
+    
+    static let regex: String = "^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[!@#$%^&*()_+=-]).{8,50}" // 8~50자리 영어/숫자/특수문자
     
     var description: String {
       switch self {
@@ -24,19 +55,14 @@ enum ValidateManager {
     }
   }
   
-  enum Password {
-    case empty
-    case invalid
-    case valid
-    
-    var description: String{
-      switch self {
-      case .empty, .valid:
-        return ""
-      case .invalid:
-        return ""
-      }
+  static func validate(password: String) -> PasswordValidate {
+    guard !password.isEmpty else {
+      return .empty
     }
+    guard password.range(of: PasswordValidate.regex, options: .regularExpression) != nil else {
+      return .invalid
+    }
+    return .valid
   }
   
 }
