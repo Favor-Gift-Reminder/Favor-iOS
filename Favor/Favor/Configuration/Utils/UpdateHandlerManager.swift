541c77b16b6615f3b013ab86a1421410a613722a
eung7 <wannabeeung@gmail.com>
2023-01-24 09:56:56 +0900

    [Feat] updateAttributes 전역 메서드 구현

--- Favor/Favor/Configuration/Utils/UpdateHandlerManager.swift
+++ Favor/Favor/Configuration/Utils/UpdateHandlerManager.swift
@@ -41,21 +41,52 @@ enum UpdateHandlerManager {
   static func onboardingHandler(_ index: Int) -> UIButton.ConfigurationUpdateHandler {
     let handler: UIButton.ConfigurationUpdateHandler = { button in
       
-      var newContainer = AttributeContainer()
-      newContainer.font = .favorFont(.bold, size: 18)
-      
       switch index {
       case 0...1:
-        newContainer.foregroundColor = .favorColor(.typo)
-        button.configuration?.baseBackgroundColor = .favorColor(.box1)
-        button.configuration?.attributedTitle = AttributedString("다음", attributes: newContainer)
+        updateAttributes(
+          button,
+          title: "다음",
+          font: .favorFont(.bold, size: 18),
+          backgroundColor: .favorColor(.box1),
+          foregroundColor: .favorColor(.typo)
+        )
       default:
-        newContainer.foregroundColor = .favorColor(.white)
-        button.configuration?.baseBackgroundColor = .favorColor(.typo)
-        button.configuration?.attributedTitle = AttributedString("계속하기", attributes: newContainer)
+        updateAttributes(
+          button,
+          title: "계속하기",
+          font: .favorFont(.bold, size: 18),
+          backgroundColor: .favorColor(.typo),
+          foregroundColor: .favorColor(.white)
+        )
       }
     }
     
     return handler
+  }
+}
+
+private extension UpdateHandlerManager {
+  static func updateAttributes(
+    _ button: UIButton,
+    title: String = "",
+    font: UIFont,
+    backgroundColor: UIColor,
+    foregroundColor: UIColor
+  ) {
+    button.configuration?.baseBackgroundColor = backgroundColor
+    
+    if !title.isEmpty {
+      var newContinaer = AttributeContainer()
+      newContinaer.foregroundColor = foregroundColor
+      newContinaer.font = font
+      button.configuration?.attributedTitle = AttributedString(title, attributes: newContinaer)
+    } else {
+      button.configuration?.titleTextAttributesTransformer = UIConfigurationTextAttributesTransformer { incoming in
+        var outgoing = incoming
+        outgoing.foregroundColor = foregroundColor
+        
+        return outgoing
+      }
+    }
   }
 }
