3309c57aa45f3bb391be178ee8df5d4bba3ee0d2
이창준 <crystalashe@naver.com>
2023-01-25 22:42:58 +0900

    [Feat] TabBarItem 디자인 적용

--- Favor/Favor/Sources/Scenes/Application/TabBar/TabBarPage.swift
+++ Favor/Favor/Sources/Scenes/Application/TabBar/TabBarPage.swift
@@ -10,25 +10,24 @@ import UIKit
 enum TabBarPage: Int, CaseIterable {
   case home = 0, reminder, myPage
   
-  // TODO: - TabBarItem configuration
   var tabBarItem: UITabBarItem {
     switch self {
     case .home:
       return UITabBarItem(
         title: "홈",
-        image: UIImage(systemName: "house.fill"),
+        image: UIImage(named: "ic_home"),
         selectedImage: nil
       )
     case .reminder:
       return UITabBarItem(
         title: "리마인더",
-        image: UIImage(systemName: "bell.fill"),
+        image: UIImage(named: "ic_notification"),
         selectedImage: nil
       )
     case .myPage:
       return UITabBarItem(
         title: "마이페이지",
-        image: UIImage(systemName: "person.circle.fill"),
+        image: UIImage(named: "ic_person_circle"),
         selectedImage: nil
       )
     }
