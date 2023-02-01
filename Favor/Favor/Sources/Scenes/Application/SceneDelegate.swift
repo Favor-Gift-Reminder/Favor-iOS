86d1859742b7f2ab13a463c3fbef81e769d1dc07
eung7 <wannabeeung@gmail.com>
2023-02-01 19:16:28 +0900

    [Fix] SceneDelegate AppFlow 메서드 제거

--- Favor/Favor/Sources/Scenes/Application/SceneDelegate.swift
+++ Favor/Favor/Sources/Scenes/Application/SceneDelegate.swift
@@ -19,8 +19,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 	func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
 		guard let windowScene = (scene as? UIWindowScene) else { return }
     
-    self.enableNavigateLog()
-    self.navigateToApp(with: windowScene)
+    let window = UIWindow(windowScene: windowScene)
+    self.window = window
+    
+    let appFlow = AppFlow(with: window)
+    self.coordinator.coordinate(flow: appFlow, with: AppStepper())
+    self.window?.makeKeyAndVisible()
 	}
 }
 
@@ -31,16 +35,4 @@ private extension SceneDelegate {
       print("➡️ Navigate to flow = \(flow) and step = \(step)")
     }).disposed(by: self.disposeBag)
   }
-  
-  func navigateToApp(with windowScene: UIWindowScene) {
-    let window = UIWindow(windowScene: windowScene)
-    self.window = window
-    
-    let appFlow = AppFlow(with: window)
-    let appStepper = AppStepper()
-    
-    self.coordinator.coordinate(flow: appFlow, with: appStepper)
-    window.makeKeyAndVisible()
-  }
-  
 }
