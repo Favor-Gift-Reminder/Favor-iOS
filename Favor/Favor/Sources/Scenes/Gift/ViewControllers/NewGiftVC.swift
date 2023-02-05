03528d42ade926b4745965b9734efdb72aa06b3a
eung7 <wannabeeung@gmail.com>
2023-02-05 17:57:47 +0900

    [Add] NewGiftVC 생성

---
+++ Favor/Favor/Sources/Scenes/Gift/ViewControllers/NewGiftVC.swift
@@ -0,0 +1,30 @@
+//
+//  NewGiftVC.swift
+//  Favor
+//
+//  Created by 김응철 on 2023/02/05.
+//
+
+import UIKit
+
+final class NewGiftViewController: BaseViewController {
+  
+  // MARK: - Properties
+  
+  
+  // MARK: - Setup
+  
+  override func setupStyles() {
+    
+  }
+  
+  override func setupLayouts() {
+    
+  }
+  
+  override func setupConstraints() {
+    
+  }
+  
+  // MARK: - Bind
+}
