abe70ac2c421782e126936d341d6b10e3be42302
이창준 <nomatterjun@gmail.com>
2023-02-21 20:33:53 +0900

    [Feat] 헤더뷰 구현

--- Favor/Favor/Sources/Scenes/MyPage/Views/MyPageHeaderView.swift
+++ Favor/Favor/Sources/Scenes/MyPage/Views/MyPageHeaderView.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 import ReactorKit
+import SnapKit
 
 final class MyPageHeaderView: StretchyCollectionHeaderView, ReuseIdentifying, View {
   
@@ -18,6 +19,36 @@ final class MyPageHeaderView: StretchyCollectionHeaderView, ReuseIdentifying, Vi
   var disposeBag = DisposeBag()
   
   // MARK: - UI Components
+
+  private lazy var profileImageView: UIImageView = {
+    let imageView = UIImageView()
+    imageView.backgroundColor = .systemBlue
+    imageView.layer.cornerRadius = 30
+    return imageView
+  }()
+
+  private lazy var nameLabel: UILabel = {
+    let label = UILabel()
+    label.textColor = .favorColor(.white)
+    label.font = .favorFont(.bold, size: 22)
+    label.text = "이름"
+    return label
+  }()
+
+  private lazy var idLabel: UILabel = {
+    let label = UILabel()
+    label.textColor = .favorColor(.line3)
+    label.font = .favorFont(.regular, size: 16)
+    label.text = "@ID123"
+    return label
+  }()
+
+  private lazy var profileStackView: UIStackView = {
+    let stackView = UIStackView()
+    stackView.axis = .vertical
+    stackView.spacing = 10
+    return stackView
+  }()
   
   // MARK: - Binding
   
@@ -32,4 +63,39 @@ final class MyPageHeaderView: StretchyCollectionHeaderView, ReuseIdentifying, Vi
   
   // MARK: - UI Setups
 
+  override func setupStyles() {
+    super.setupStyles()
+  }
+
+  override func setupLayouts() {
+    super.setupLayouts()
+
+    [
+      self.profileImageView,
+      self.profileStackView
+    ].forEach {
+      self.addSubview($0)
+    }
+
+    [
+      self.nameLabel,
+      self.idLabel
+    ].forEach {
+      self.profileStackView.addArrangedSubview($0)
+    }
+  }
+
+  override func setupConstraints() {
+    super.setupConstraints()
+
+    self.profileImageView.snp.makeConstraints { make in
+      make.leading.equalToSuperview().inset(20)
+      make.bottom.equalToSuperview().inset(58)
+      make.width.height.equalTo(60)
+    }
+    self.profileStackView.snp.makeConstraints { make in
+      make.leading.equalTo(self.profileImageView.snp.trailing).offset(16)
+      make.centerY.equalTo(self.profileImageView.snp.centerY)
+    }
+  }
 }
