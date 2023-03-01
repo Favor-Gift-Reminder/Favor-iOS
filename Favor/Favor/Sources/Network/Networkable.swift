08e563ba8bf4a22d1bb1946de1809aee6c5dddf3
이창준 <nomatterjun@gmail.com>
2023-03-01 13:51:24 +0900

    [Chore] Networkable 코멘트 추가

--- Favor/Favor/Sources/Network/Networkable.swift
+++ Favor/Favor/Sources/Network/Networkable.swift
@@ -8,6 +8,19 @@
 import Moya
 
 /// `MoyaProvider` 객체를 생성하여 반환하는 작업을 지원하는 프로토콜
+///
+/// **Example**
+/// ```
+/// struct UserAPI: Networkable {
+///   typealias Target = UserTarget
+///
+///   static func fetchUserRequest(request: UserRequest, completion: @escaping (_ succeed: User?, _ failed: Error?) -> Void {
+///     self.makeProvider().request(.one(request)) { result in
+///       // Handle response
+///     }
+///   }
+/// }
+/// ```
 protocol Networkable {
   associatedtype Target: TargetType
   static func makeProvider() -> MoyaProvider<Target>
