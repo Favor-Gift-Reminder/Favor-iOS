5ed14d5fe10eee4ec8a48ce550094c364cbe3870
이창준 <nomatterjun@gmail.com>
2023-03-11 02:36:02 +0900

    [Chore] 사용되지 않는 Test Code 삭제

--- Favor/FavorNetworkKit/Tests/FavorNetworkKitTests/FavorNetworkKitTests.swift
+++ Favor/FavorNetworkKit/Tests/FavorNetworkKitTests/FavorNetworkKitTests.swift
@@ -1,11 +1,10 @@
-import XCTest
 @testable import FavorNetworkKit
+import XCTest
 
 final class FavorNetworkKitTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(FavorNetworkKit().text, "Hello, World!")
-    }
+  func testExample() throws {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct
+    // results.
+  }
 }
