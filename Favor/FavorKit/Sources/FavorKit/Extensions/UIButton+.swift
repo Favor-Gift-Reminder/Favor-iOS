1cca7d74aecce43179b892e7c2ecd5c6cbb90b62
이창준 <nomatterjun@gmail.com>
2023-03-13 17:40:20 +0900

    [Feat] Configuration을 사용한 UIButton의 AttributedTitle을 업데이트하는 extension 메서드 구현

--- Favor/FavorKit/Sources/FavorKit/Extensions/UIButton+.swift
+++ Favor/FavorKit/Sources/FavorKit/Extensions/UIButton+.swift
@@ -10,6 +10,18 @@ import UIKit
 import RxCocoa
 import RxSwift
 
+extension UIButton.Configuration {
+
+  public mutating func updateAttributedTitle(_ text: String, font: UIFont) {
+    var container = AttributeContainer()
+    container.font = font
+    self.attributedTitle = AttributedString(
+      text,
+      attributes: container
+    )
+  }
+}
+
 public extension Reactive where Base: UIButton {
   var tapWithHaptic: ControlEvent<Void> {
     HapticManager.haptic(style: .soft)
