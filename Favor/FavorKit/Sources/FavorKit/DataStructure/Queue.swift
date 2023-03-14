13376bc77543cc0204356faf9c43dcd0e7bccfe2
이창준 <nomatterjun@gmail.com>
2023-03-14 17:53:12 +0900

    [Feat] Queue 구현

---
+++ Favor/FavorKit/Sources/FavorKit/DataStructure/Queue.swift
@@ -0,0 +1,33 @@
+//
+//  Queue.swift
+//  Favor
+//
+//  Created by 이창준 on 2023/03/14.
+//
+
+struct Queue<T> {
+  private var queue: [T?] = []
+  private var front: Int = 0
+
+  public var frontIndex: Int { return self.front }
+
+  public var size: Int { return self.queue.count }
+
+  public var isEmpty: Bool { return self.queue[self.front] == nil }
+
+  public mutating func enqueue(_ element: T) {
+    self.queue.append(element)
+  }
+
+  @discardableResult
+  public mutating func dequeue() -> T? {
+    guard
+      self.front <= self.size,
+      let element = self.queue[self.front]
+    else { return nil }
+    
+    self.queue[self.front] = nil
+    self.front += 1
+    return element
+  }
+}
