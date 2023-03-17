42bf2b61a6bbb8e7f80d6a988a574c4ee2dc785a
eung7 <wannabeeung@gmail.com>
2023-03-17 09:46:07 +0900

    [Feat] BaseViewController+ 구현

---
+++ Favor/FavorKit/Sources/FavorKit/Extensions/BaseViewController+.swift
@@ -0,0 +1,36 @@
+//
+//  BaseViewController+.swift
+//  
+//
+//  Created by 김응철 on 2023/03/17.
+//
+
+import UIKit
+
+import RxSwift
+
+private var spinnerView: UIView?
+
+extension Reactive where Base: BaseViewController {
+  
+  public var spinnerState: Binder<Bool> {
+    return Binder(self.base) { vc, isLoading in
+      let spinner = UIActivityIndicatorView()
+      spinner.color = .lightGray
+      
+      if isLoading {
+        guard spinnerView == nil else { return }
+        spinnerView = UIView(frame: vc.view.bounds)
+        spinnerView?.backgroundColor = .clear
+        spinnerView?.addSubview(spinner)
+        spinner.center = spinnerView!.center
+        spinner.startAnimating()
+        vc.view.addSubview(spinnerView!)
+      } else {
+        guard spinnerView != nil else { return }
+        spinnerView?.removeFromSuperview()
+        spinnerView = nil
+      }
+    }
+  }
+}
