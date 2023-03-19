020c5839224a7331d5c24f5f96e9d0c200678115
이창준 <nomatterjun@gmail.com>
2023-03-19 20:58:45 +0900

    [Feat] 친구 Object 수정

--- Favor/Favor/Sources/Models/Realm/Friend.swift
+++ Favor/Favor/Sources/Models/Realm/Friend.swift
@@ -16,38 +16,46 @@ public class Friend: Object {
   /// 친구를 보유한 회원의 회원 번호
   @Persisted(originProperty: "friendList") var userNo: LinkingObjects<User>
   /// 친구 이름
-  @Persisted var friendName: String
+  @Persisted var name: String
   /// 친구 사진
-  @Persisted var friendPhoto: Photo?
+  @Persisted var profilePhoto: Photo?
   /// 친구에 대한 메모
-  @Persisted var friendMemo: String?
+  @Persisted var memo: String?
   /// 친구가 회원일 경우, 해당 친구의 회원 번호
   @Persisted var friendUserNo: Int?
   /// 친구의 회원 여부 (회원 = `true`)
   @Persisted var isUser: Bool
 
+  public override class func propertiesMapping() -> [String: String] {
+    [
+      "name": "friendName",
+      "profilePhoto": "friendPhoto",
+      "memo": "friendMemo"
+    ]
+  }
+
   // MARK: - Initializer
 
   /// - Parameters:
   ///   - friendNo: ***PK*** 친구 번호
-  ///   - friendName: 친구 이름
-  ///   - friendPhoto: 친구 사진
-  ///   - friendMemo: 친구에 대한 메모
+  ///   - name: 친구 이름
+  ///   - profilePhoto: 친구 사진
+  ///   - memo: 친구에 대한 메모
   ///   - friendUserNo: 친구가 회원일 경우, 해당 친구의 회원 번호
   ///   - isUser: 친구의 회원 여부 (true: 회원)
   public convenience init(
     friendNo: Int,
-    friendName: String,
-    friendPhoto: Photo? = nil,
-    friendMemo: String? = nil,
+    name: String,
+    profilePhoto: Photo? = nil,
+    memo: String? = nil,
     friendUserNo: Int? = nil,
     isUser: Bool
   ) {
     self.init()
     self.friendNo = friendNo
-    self.friendName = friendName
-    self.friendPhoto = friendPhoto
-    self.friendMemo = friendMemo
+    self.name = name
+    self.profilePhoto = profilePhoto
+    self.memo = memo
     self.friendUserNo = friendUserNo
     self.isUser = isUser
   }
