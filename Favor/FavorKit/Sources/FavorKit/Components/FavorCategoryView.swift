e17cb60532c693972ed910c1e21ae1ac82c27ef6
eung7 <wannabeeung@gmail.com>
2023-03-21 10:30:15 +0900

    [Feat] CategoryView UI업데이트 및 Subject 추가 구현

--- Favor/FavorKit/Sources/FavorKit/Components/FavorCategoryView.swift
+++ Favor/FavorKit/Sources/FavorKit/Components/FavorCategoryView.swift
@@ -7,54 +7,153 @@
 
 import UIKit
 
+import RxCocoa
+import RxSwift
 import SnapKit
 
 public final class FavorCategoryView: UIScrollView {
   
-  // MARK: - Properties
+  // MARK: - UI COMPONENTS
   
-  private let lightGiftButton = FavorSmallButton(with: .mainWithIcon("가벼운 선물", imageName: ""))
-  private let birthButton = FavorSmallButton(with: .mainWithIcon("생일", imageName: ""))
-  private let houseWarmingButton = FavorSmallButton(with: .mainWithIcon("집들이", imageName: ""))
-  private let testButton = FavorSmallButton(with: .mainWithIcon("시험", imageName: ""))
-  private let promotionButton = FavorSmallButton(with: .mainWithIcon("승진", imageName: ""))
-  private let graduationButton = FavorSmallButton(with: .mainWithIcon("졸업", imageName: ""))
-  private let etcButton = FavorSmallButton(with: .mainWithIcon("기타", imageName: ""))
+  private lazy var lightGiftButton = self.button("가벼운 선물", selectedImage: nil)
+  private lazy var birthDayButton = self.button("생일", selectedImage: .favorIcon(.congrat))
+  private lazy var houseWarmButton = self.button("집들이", selectedImage: .favorIcon(.housewarm))
+  private lazy var testButton = self.button("시험", selectedImage: nil)
+  private lazy var promotionButton = self.button("승진", selectedImage: .favorIcon(.employed))
+  private lazy var graduationButton = self.button("졸업", selectedImage: .favorIcon(.graduate))
+  private lazy var etcButton = self.button("기타", selectedImage: nil)
   
   private let contentsView: UIView = {
     let view = UIView()
-    view.backgroundColor = .favorColor(.background)
+    view.backgroundColor = .favorColor(.white)
     
     return view
   }()
   
-  // MARK: - Initializer
+  // MARK: - PROPERTIES
+  
+  private let disposeBag = DisposeBag()
+  
+  /// 현재 선택된 버튼 이벤트를 방출하는 Subject입니다.
+  private(set) var currentSelectedButton = BehaviorSubject<Category>(value: .lightGift)
+  
+  // MARK: - INITIALIZER
   
   override init(frame: CGRect) {
     super.init(frame: frame)
     setupStyles()
     setupLayouts()
     setupConstraints()
+    bind()
   }
   
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
+  
+  // MARK: - BIND
+  
+  private func bind() {
+
+    // 가벼운 선물 버튼 클릭
+    self.lightGiftButton.rx.tap
+      .bind(with: self) { owner, _ in
+        owner.changeButtonState(.lightGift)
+      }
+      .disposed(by: self.disposeBag)
+    
+    // 생일 버튼 클릭
+    self.birthDayButton.rx.tap
+      .bind(with: self) { owner, _ in
+        owner.changeButtonState(.birthDay)
+      }
+      .disposed(by: self.disposeBag)
+    
+    // 집들이 버튼 클릭
+    self.houseWarmButton.rx.tap
+      .bind(with: self) { owner, _ in
+        owner.changeButtonState(.houseWarm)
+      }
+      .disposed(by: self.disposeBag)
+
+    // 시험 버튼 클릭
+    self.testButton.rx.tap
+      .bind(with: self) { owner, _ in
+        owner.changeButtonState(.test)
+      }
+      .disposed(by: self.disposeBag)
+
+    // 승진 버튼 클릭
+    self.promotionButton.rx.tap
+      .bind(with: self) { owner, _ in
+        owner.changeButtonState(.promotion)
+      }
+      .disposed(by: self.disposeBag)
+
+    // 졸업 버튼 클릭
+    self.graduationButton.rx.tap
+      .bind(with: self) { owner, _ in
+        owner.changeButtonState(.graduation)
+      }
+      .disposed(by: self.disposeBag)
+    
+    // 기타 버튼 클릭
+    self.etcButton.rx.tap
+      .bind(with: self) { owner, _ in
+        owner.changeButtonState(.etc)
+      }
+      .disposed(by: self.disposeBag)
+  }
+  
+  // MARK: - HELPERS
+  
+  private func changeButtonState(_ category: Category) {
+    [
+      self.lightGiftButton,
+      self.birthDayButton,
+      self.houseWarmButton,
+      self.testButton,
+      self.promotionButton,
+      self.graduationButton,
+      self.etcButton
+    ].forEach {
+      $0.isSelected = false
+    }
+    
+    switch category {
+    case .lightGift:
+      self.lightGiftButton.isSelected = true
+    case .birthDay:
+      self.birthDayButton.isSelected = true
+    case .houseWarm:
+      self.houseWarmButton.isSelected = true
+    case .test:
+      self.testButton.isSelected = true
+    case .promotion:
+      self.promotionButton.isSelected = true
+    case .graduation:
+      self.graduationButton.isSelected = true
+    case .etc:
+      self.etcButton.isSelected = true
+    }
+    
+    self.currentSelectedButton.onNext(category)
+  }
 }
 
-extension FavorCategoryView {
-  func setupStyles() {
-    self.backgroundColor = .favorColor(.background)
+extension FavorCategoryView: BaseView {
+  public func setupStyles() {
+    self.backgroundColor = .favorColor(.white)
     self.showsHorizontalScrollIndicator = false
   }
   
-  func setupLayouts() {
+  public func setupLayouts() {
     self.addSubview(self.contentsView)
     
     [
       self.lightGiftButton,
-      self.birthButton,
-      self.houseWarmingButton,
+      self.birthDayButton,
+      self.houseWarmButton,
       self.testButton,
       self.promotionButton,
       self.graduationButton,
@@ -64,7 +163,7 @@ extension FavorCategoryView {
     }
   }
   
-  func setupConstraints() {
+  public func setupConstraints() {
     self.snp.makeConstraints { make in
       make.height.equalTo(32)
     }
@@ -79,18 +178,18 @@ extension FavorCategoryView {
       make.top.bottom.equalToSuperview()
     }
     
-    self.birthButton.snp.makeConstraints { make in
+    self.birthDayButton.snp.makeConstraints { make in
       make.leading.equalTo(self.lightGiftButton.snp.trailing).offset(10)
       make.top.bottom.equalToSuperview()
     }
     
-    self.houseWarmingButton.snp.makeConstraints { make in
-      make.leading.equalTo(self.birthButton.snp.trailing).offset(10)
+    self.houseWarmButton.snp.makeConstraints { make in
+      make.leading.equalTo(self.birthDayButton.snp.trailing).offset(10)
       make.top.bottom.equalToSuperview()
     }
     
     self.testButton.snp.makeConstraints { make in
-      make.leading.equalTo(self.houseWarmingButton.snp.trailing).offset(10)
+      make.leading.equalTo(self.houseWarmButton.snp.trailing).offset(10)
       make.top.bottom.equalToSuperview()
     }
     
@@ -109,5 +208,27 @@ extension FavorCategoryView {
       make.top.bottom.equalToSuperview()
       make.trailing.equalToSuperview()
     }
+  }
+}
+
+private extension FavorCategoryView {
+  func button(_ title: String, selectedImage: UIImage?) -> FavorSmallButton {
+    let btn = FavorSmallButton(with: .gray(title))
+    
+    btn.configurationUpdateHandler = {
+      switch $0.state {
+      case .normal:
+        $0.configuration = FavorSmallButtonType.gray(title).configuration
+      case .selected:
+        let image = selectedImage?.withTintColor(.favorColor(.white))
+        $0.configuration = FavorSmallButtonType.darkWithIcon(
+          title,
+          image: image
+        ).configuration
+      default:
+        break
+      }
+    }
+    return btn
   }
 }
