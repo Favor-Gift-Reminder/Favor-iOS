27f4bea02045a111ba9f54d5246187198ec2dc9c
이창준 <nomatterjun@gmail.com>
2023-03-23 19:53:10 +0900

    [Chore] Fetcher의 각 과정에 코멘트 작성

--- Favor/FavorNetworkKit/Sources/FavorNetworkKit/Fetcher/Fetcher.swift
+++ Favor/FavorNetworkKit/Sources/FavorNetworkKit/Fetcher/Fetcher.swift
@@ -46,18 +46,26 @@ public class Fetcher<T> {
 
     let disposeBag = DisposeBag()
 
+    // 로컬에 저장된 데이터를 방출하며 status를 inProgress로 설정합니다.
     onNext(.inProgress, _onLocal())
 
+    // 서버로부터 데이터를 받아와 처리합니다.
     self.onRemote()
+      // 성공했다면
       .subscribe(onSuccess: { data in
+        // 로컬 데이터를 업데이트하고
         self.onLocalUpdate(data)
 
+        // 로컬 데이터를 방출하며
         self.onLocalByObservable()
           .subscribe(onNext: {
+            // status를 success로 설정합니다.
             onNext(.success, $0)
           })
           .disposed(by: disposeBag)
+      // 실패했다면
       }, onFailure: { _ in
+        // 로컬 데이터를 그대로 방출하고 status를 failure로 설정합니다.
         onNext(.failure, _onLocal())
       })
       .disposed(by: disposeBag)
