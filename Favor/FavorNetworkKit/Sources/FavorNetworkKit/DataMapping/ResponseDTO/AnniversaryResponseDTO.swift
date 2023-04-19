67e74eac46830dccb12f9219646dd27d5befcef7
이창준 <nomatterjun@gmail.com>
2023-04-19 17:45:13 +0900

    [Feat] 적용 안된 파일 커밋

--- Favor/FavorNetworkKit/Sources/FavorNetworkKit/DataMapping/ResponseDTO/AnniversaryResponseDTO.swift
+++ Favor/FavorNetworkKit/Sources/FavorNetworkKit/DataMapping/ResponseDTO/AnniversaryResponseDTO.swift
@@ -13,4 +13,23 @@ public struct AnniversaryResponseDTO: Decodable {
   public let anniversaryTitle: String
   public let isPinned: Bool
   public let userNo: Int
+
+  private enum CodingKeys: CodingKey {
+    case anniversaryDate
+    case anniversaryNo
+    case anniversaryTitle
+    case isPinned
+    case userNo
+  }
+
+  public init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+    let anniversaryDateString = try container.decode(String.self, forKey: .anniversaryDate)
+    let anniversaryDate = anniversaryDateString.toDate("yyyy-MM-dd")
+    self.anniversaryDate = anniversaryDate ?? .distantPast
+    self.anniversaryNo = try container.decode(Int.self, forKey: .anniversaryNo)
+    self.anniversaryTitle = try container.decode(String.self, forKey: .anniversaryTitle)
+    self.isPinned = try container.decode(Bool.self, forKey: .isPinned)
+    self.userNo = try container.decode(Int.self, forKey: .userNo)
+  }
 }
