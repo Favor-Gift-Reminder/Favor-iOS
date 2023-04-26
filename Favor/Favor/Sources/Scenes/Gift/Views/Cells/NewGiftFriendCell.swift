5515f5a8b39a15c3c4e8a2acd01bd33b7f4eaa6f
eung7 <wannabeeung@gmail.com>
2023-04-26 09:04:53 +0900

    [Feat] NewGiftFriendCell 구현

--- Favor/Favor/Sources/Scenes/Gift/Views/Cells/NewGiftFriendCell.swift
+++ Favor/Favor/Sources/Scenes/Gift/Views/Cells/NewGiftFriendCell.swift
@@ -15,52 +15,69 @@ final class NewGiftFriendCell: BaseFriendCell, View, Reusable {
   
   enum RightButtonType {
     case add
-    case check
-    case close
+    case done
+    case remove
   }
   
   // MARK: - UI Components
   
   private let rightImageView: UIImageView = {
     let iv = UIImageView()
-    iv.tintColor = .favorColor(.divider)
+    iv.image = .favorIcon(.newGift)?.withTintColor(.favorColor(.line3))
     return iv
   }()
+  
+  // MARK: - Properties
+  
+  var currentButtonType: RightButtonType = .add {
+    didSet {
+      let image: UIImage?
+      switch currentButtonType {
+      case .add:
+        image = .favorIcon(.newGift)?.withTintColor(.favorColor(.line3))
+      case .remove:
+        image = .favorIcon(.remove)?.withTintColor(.favorColor(.line3))
+      case .done:
+        image = .favorIcon(.done)?.withTintColor(.favorColor(.line3))
+      }
+      self.rightImageView.image = image
+    }
+  }
   
   // MARK: - Bind
   
   func bind(reactor: NewGiftFriendCellReactor) {
-    reactor.state.map { $0.friendName }
-      .asDriver(onErrorJustReturn: "")
-      .drive(with: self) { $0.friendName = $1 }
-      .disposed(by: self.disposeBag)
-    
-    reactor.state.map { $0.profileImage }
-      .asDriver(onErrorJustReturn: .undefined)
+    reactor.state.map { $0.friend }
+      .asDriver(onErrorRecover: { _ in return .empty() })
       .drive(with: self) {
-        switch $1 {
-        case .undefined:
-          $0.cellType = .undefined
-        case .user(let image):
-          $0.cellType = .user(image)
-        }
+        $0.friendName = $1.name
+        // TODO: 이미지 설정
       }
       .disposed(by: self.disposeBag)
     
     reactor.state.map { $0.rightButtonState }
       .asDriver(onErrorJustReturn: .add)
       .drive(with: self) {
-        let icon: UIImage?
-        switch $1 {
-        case .add:
-          icon = .favorIcon(.add)
-        case .check:
-          icon = .favorIcon(.check)
-        case .close:
-          icon = .favorIcon(.close)
-        }
-        $0.rightImageView.image = icon
+        $0.currentButtonType = $1
       }
       .disposed(by: self.disposeBag)
+  }
+  
+  // MARK: - Setup
+  
+  override func setupLayouts() {
+    super.setupLayouts()
+    
+    self.contentView.addSubview(self.rightImageView)
+  }
+  
+  override func setupConstraints() {
+    super.setupConstraints()
+    
+    self.rightImageView.snp.makeConstraints { make in
+      make.width.height.equalTo(18.0)
+      make.centerY.equalToSuperview()
+      make.trailing.equalToSuperview().inset(25.0)
+    }
   }
 }
