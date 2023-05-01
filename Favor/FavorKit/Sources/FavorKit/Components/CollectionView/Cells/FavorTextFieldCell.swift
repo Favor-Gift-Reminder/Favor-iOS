9889dea080bf77906956e80bcdec29b08d6d2476
이창준 <nomatterjun@gmail.com>
2023-05-01 17:32:10 +0900

    [Chore] FavorKit에 Reusable dependency 추가

---
+++ Favor/FavorKit/Sources/FavorKit/Components/CollectionView/Cells/FavorTextFieldCell.swift
@@ -0,0 +1,14 @@
+//
+//  FavorTextFieldCell.swift
+//  Favor
+//
+//  Created by 이창준 on 2023/05/01.
+//
+
+import UIKit
+
+import Reusable
+
+open class FavorTextFieldCell: BaseCollectionViewCell, Reusable {
+
+}
