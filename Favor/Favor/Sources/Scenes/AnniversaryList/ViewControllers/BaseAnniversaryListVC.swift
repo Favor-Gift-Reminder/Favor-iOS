79b45c38788e48728e6656deca4cfa3b3f6ec518
이창준 <nomatterjun@gmail.com>
2023-05-18 15:52:45 +0900

    [Feat] 편집 리스트까지의 UI 구현

---
+++ Favor/Favor/Sources/Scenes/AnniversaryList/ViewControllers/BaseAnniversaryListVC.swift
@@ -0,0 +1,118 @@
+//
+//  BaseAnniversaryListVC.swift
+//  Favor
+//
+//  Created by 이창준 on 2023/05/17.
+//
+
+import UIKit
+
+import FavorKit
+
+public class BaseAnniversaryListViewController: BaseViewController {
+  public typealias AnniversaryListDataSource = UICollectionViewDiffableDataSource<AnniversaryListSection, AnniversaryListSectionItem>
+
+  // MARK: - Properties
+  
+  public lazy var dataSource: AnniversaryListDataSource = {
+    let dataSource = AnniversaryListDataSource(
+      collectionView: self.collectionView,
+      cellProvider: { [weak self] collectionView, indexPath, item in
+        switch item {
+        case .empty:
+          let cell = collectionView.dequeueReusableCell(for: indexPath) as FavorEmptyCell
+          cell.bindEmptyData(image: nil, text: "내 기념일을 등록해보세요.")
+          return cell
+        case .anniversary(let reactor):
+          let cell = collectionView.dequeueReusableCell(for: indexPath) as AnniversaryListCell
+          cell.reactor = reactor
+          cell.cellType = .anniversary
+
+          cell.rx.rightButtonDidTap
+            .asDriver(onErrorRecover: { _ in return .empty()})
+            .drive(with: cell, onNext: { _, _ in
+              guard let self = self else { return }
+              self.handleAnniversaryData(reactor.currentState.anniversary)
+            })
+            .disposed(by: cell.disposeBag)
+
+          return cell
+        }
+      }
+    )
+    dataSource.supplementaryViewProvider = { collectionView, kind, indexPath in
+      switch kind {
+      case UICollectionView.elementKindSectionHeader:
+        let header = collectionView.dequeueReusableSupplementaryView(
+          ofKind: kind,
+          for: indexPath
+        ) as AnniversaryListSectionHeaderView
+        let currentSnapshot = self.dataSource.snapshot()
+        let section = currentSnapshot.sectionIdentifiers[indexPath.section]
+        let numberOfItems = currentSnapshot.numberOfItems(inSection: section)
+        let title = indexPath.section == .zero ? "고정됨" : "전체"
+        header.bind(title: title, numberOfFriends: numberOfItems)
+        return header
+      default:
+        return UICollectionReusableView()
+      }
+    }
+    return dataSource
+  }()
+
+  // MARK: - UI Components
+
+  public lazy var adapter: Adapter<AnniversaryListSection, AnniversaryListSectionItem> = {
+    let adapter = Adapter(collectionView: self.collectionView, dataSource: self.dataSource)
+    adapter.configuration = Adapter.Configuration(
+      scrollDirection: .vertical,
+      sectionSpacing: 40
+    )
+    return adapter
+  }()
+
+  public lazy var collectionView: UICollectionView = {
+    let collectionView = UICollectionView(
+      frame: .zero,
+      collectionViewLayout: UICollectionViewLayout()
+    )
+
+    // Register
+    collectionView.register(cellType: FavorEmptyCell.self)
+    collectionView.register(cellType: AnniversaryListCell.self)
+    collectionView.register(
+      supplementaryViewType: AnniversaryListSectionHeaderView.self,
+      ofKind: UICollectionView.elementKindSectionHeader
+    )
+
+    collectionView.showsHorizontalScrollIndicator = false
+    collectionView.contentInset = UIEdgeInsets(top: 32, left: .zero, bottom: .zero, right: .zero)
+    collectionView.contentInsetAdjustmentBehavior = .never
+    return collectionView
+  }()
+
+  // MARK: - Life Cycle
+
+  public override func viewDidLoad() {
+    super.viewDidLoad()
+
+    self.adapter.adapt()
+  }
+
+  // MARK: - Functions
+
+  public func handleAnniversaryData(_ anniversary: Anniversary) { }
+
+  // MARK: - UI Setups
+
+  public override func setupLayouts() {
+    self.view.addSubview(self.collectionView)
+  }
+
+  public override func setupConstraints() {
+    self.collectionView.snp.makeConstraints { make in
+      make.directionalVerticalEdges.equalTo(self.view.safeAreaLayoutGuide)
+      make.directionalHorizontalEdges.equalTo(self.view.layoutMarginsGuide)
+    }
+  }
+}
