8c5311fac1f2ca4b9b387d5969c2f5ff61b38dbd
이창준 <nomatterjun@gmail.com>
2023-05-23 01:52:12 +0900

    [Chore] 토스트 메시지 기념일 이름 강조

--- Favor/FavorKit/Sources/FavorKit/Components/FavorToastMessageView/ToastMessage.swift
+++ Favor/FavorKit/Sources/FavorKit/Components/FavorToastMessageView/ToastMessage.swift
@@ -24,11 +24,11 @@ extension ToastMessage {
   public var description: String? {
     switch self {
     case .anniversaryAdded(let anniversaryTitle):
-      return "\(anniversaryTitle) 추가 완료!"
+      return "\"\(anniversaryTitle)\" 추가 완료!"
     case .anniversaryModifed(let anniversaryTitle):
-      return "\(anniversaryTitle) 수정 완료!"
+      return "\"\(anniversaryTitle)\" 수정 완료!"
     case .anniversaryDeleted(let anniversaryTitle):
-      return "\(anniversaryTitle) 삭제 완료!"
+      return "\"\(anniversaryTitle)\" 삭제 완료!"
 
     case .custom(let text):
       return text
