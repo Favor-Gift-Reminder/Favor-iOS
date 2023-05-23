2d0da3bbf74e109a9b6e713788436f88c2848646
이창준 <nomatterjun@gmail.com>
2023-05-23 19:44:27 +0900

    [Feat] ReminderResponseDTO toDomain 함수 추가

--- Favor/FavorNetworkKit/Sources/FavorNetworkKit/DataMapping/ResponseDTO/ReminderResponseDTO.swift
+++ Favor/FavorNetworkKit/Sources/FavorNetworkKit/DataMapping/ResponseDTO/ReminderResponseDTO.swift
@@ -7,11 +7,13 @@
 
 import Foundation
 
+import FavorKit
+
 public struct ReminderResponseDTO: Decodable {
   public let alarmTime: Date?
   public let friendNo: Int
   public let isAlarmSet: Bool
-  public let memo: String
+  public let memo: String?
   public let reminderDate: Date
   public let reminderNo: Int
   public let reminderTitle: String
@@ -35,12 +37,28 @@ public struct ReminderResponseDTO: Decodable {
     self.alarmTime = alarmTime
     self.friendNo = try container.decode(Int.self, forKey: .friendNo)
     self.isAlarmSet = try container.decode(Bool.self, forKey: .isAlarmSet)
-    self.memo = try container.decode(String.self, forKey: .memo)
+    self.memo = try container.decodeIfPresent(String.self, forKey: .memo)
     let reminderDateString = try container.decode(String.self, forKey: .reminderDate)
     let reminderDate = reminderDateString.toDate("yyyy-MM-dd") ?? .distantPast
     self.reminderDate = reminderDate
     self.reminderNo = try container.decode(Int.self, forKey: .reminderNo)
     self.reminderTitle = try container.decode(String.self, forKey: .reminderTitle)
     self.userNo = try container.decode(Int.self, forKey: .userNo)
+  }
+}
+
+// MARK: - Convert
+
+extension ReminderResponseDTO {
+  public func toDomain() -> Reminder {
+    return Reminder(
+      reminderNo: self.reminderNo,
+      title: self.reminderTitle,
+      date: self.reminderDate,
+      memo: self.memo,
+      shouldNotify: self.isAlarmSet,
+      notifyTime: self.alarmTime,
+      friendNo: self.friendNo
+    )
   }
 }
