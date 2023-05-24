1704f182af1d286946a9ed545e330f760dc8c3b4
이창준 <nomatterjun@gmail.com>
2023-05-25 00:06:04 +0900

    [Chore] 에러 임시 처리

--- Favor/FavorKit/Sources/FavorKit/Base/BaseBottomSheet.swift
+++ Favor/FavorKit/Sources/FavorKit/Base/BaseBottomSheet.swift
@@ -1,6 +1,6 @@
 //
 //  BaseBottomSheet.swift
-//  
+//  Favor
 //
 //  Created by 김응철 on 2023/03/31.
 //
@@ -8,33 +8,40 @@
 import UIKit
 
 import SnapKit
+import Then
 
 open class BaseBottomSheet: BaseViewController {
-  
+
+  enum Metric {
+    static let bottomSheetHeight: CGFloat = 294.0
+    static let dismissibleHeight: CGFloat = 200.0
+  }
+
   // MARK: - UI
-  
+
   public let containerView: UIView = {
     let view = UIView()
     view.backgroundColor = .favorColor(.white)
     view.layer.cornerRadius = 24
+    view.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
     view.clipsToBounds = true
     return view
   }()
-  
+
   private let dimmedView: UIView = {
     let view = UIView()
     view.backgroundColor = .favorColor(.black)
     view.alpha = 0.6
     return view
   }()
-  
+
   public let titleLabel: UILabel = {
     let lb = UILabel()
     lb.textColor = .favorColor(.icon)
     lb.font = .favorFont(.bold, size: 18)
     return lb
   }()
-  
+
   public lazy var cancelButton: UIButton = {
     let btn = UIButton()
     let attributedTitle = NSAttributedString(
@@ -48,7 +55,24 @@ open class BaseBottomSheet: BaseViewController {
     btn.addTarget(self, action: #selector(dismissBottomSheet), for: .touchUpInside)
     return btn
   }()
-  
+
+   public let finishButton: UIButton = UIButton().then {
+    var config = UIButton.Configuration.plain()
+    var container = AttributeContainer()
+    container.font = .favorFont(.bold, size: 18)
+    config.attributedTitle = AttributedString("완료", attributes: container)
+    config.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
+    $0.configuration = config
+    $0.configurationUpdateHandler = {
+      switch $0.state {
+      case .disabled:
+        $0.configuration?.baseForegroundColor = .favorColor(.line2)
+      default:
+        $0.configuration?.baseForegroundColor = .favorColor(.icon)
+      }
+    }
+  }
+
   private lazy var tapGesture: UITapGestureRecognizer = {
     let tg = UITapGestureRecognizer(
       target: self,
@@ -56,7 +80,7 @@ open class BaseBottomSheet: BaseViewController {
     )
     return tg
   }()
-  
+
   private lazy var panGesture: UIPanGestureRecognizer = {
     let pg = UIPanGestureRecognizer(
       target: self,
@@ -66,30 +90,28 @@ open class BaseBottomSheet: BaseViewController {
     pg.delaysTouchesEnded = false
     return pg
   }()
-  
+
   // MARK: - PROPERTIES
-  
-  private var containerViewHeight: Constraint?
-  private var containerViewBottomInset: Constraint?
-  private let maxHeight: CGFloat = 294
-  private let dismissibleHeight: CGFloat = 200
-  
+
+  public var containerViewHeight: Constraint?
+  public var containerViewBottomInset: Constraint?
+
   // MARK: - LIFE CYCLE
-  
+
   public override func viewDidAppear(_ animated: Bool) {
     super.viewDidAppear(animated)
     self.animateShowDimmedView()
     self.animatePresentContainerView()
   }
-  
+
   // MARK: - SETUP
-  
+
   open override func setupStyles() {
     self.view.backgroundColor = .clear
     self.dimmedView.addGestureRecognizer(self.tapGesture)
     self.view.addGestureRecognizer(self.panGesture)
   }
-  
+
   open override func setupLayouts() {
     [
       self.dimmedView,
@@ -97,88 +119,83 @@ open class BaseBottomSheet: BaseViewController {
     ].forEach {
       self.view.addSubview($0)
     }
-    
+
     [
       self.titleLabel,
       self.cancelButton,
+      self.finishButton
     ].forEach {
       self.containerView.addSubview($0)
     }
   }
-  
+
   open override func setupConstraints() {
     self.dimmedView.snp.makeConstraints { make in
       make.edges.equalToSuperview()
     }
-    
+
     self.containerView.snp.makeConstraints { make in
       make.leading.trailing.equalToSuperview()
-      self.containerViewBottomInset = make.bottom.equalToSuperview().inset(-self.maxHeight).constraint
-      self.containerViewHeight = make.height.equalTo(self.maxHeight).constraint
+      self.containerViewBottomInset = make.bottom.equalToSuperview()
+        .inset(-Metric.bottomSheetHeight)
+        .constraint
+      self.containerViewHeight = make.height.equalTo(Metric.bottomSheetHeight).constraint
     }
-    
+
     self.titleLabel.snp.makeConstraints { make in
       make.top.equalToSuperview().inset(20)
       make.centerX.equalToSuperview()
     }
-    
+
     self.cancelButton.snp.makeConstraints { make in
       make.leading.equalToSuperview().inset(20)
       make.centerY.equalTo(self.titleLabel)
     }
+
+    self.finishButton.snp.makeConstraints { make in
+      make.trailing.equalToSuperview().inset(20)
+      make.centerY.equalTo(self.titleLabel)
+    }
   }
-  
+
   // MARK: - SELECTORS
-  
+
   @objc
-  private func dismissBottomSheet() {
+  open func dismissBottomSheet() {
     self.animateDismissView()
   }
-  
+
   @objc
   private func handlePanGesture(_ gesture: UIPanGestureRecognizer) {
     let translation = gesture.translation(in: self.view)
-    let newHeight = self.maxHeight - translation.y
-    
+    let newHeight = Metric.bottomSheetHeight - translation.y
+
     switch gesture.state {
     case .changed:
-      if newHeight < self.maxHeight {
+      if newHeight < Metric.bottomSheetHeight {
         self.containerViewHeight?.update(offset: newHeight)
         self.view.layoutIfNeeded()
       }
     case .ended:
-      if newHeight < self.dismissibleHeight {
+      if newHeight < Metric.dismissibleHeight {
         self.animateDismissView()
       } else {
-        self.animateContainerHeight(self.maxHeight)
+        self.animateContainerHeight(Metric.bottomSheetHeight)
       }
     default:
       break
     }
   }
-  
+
   // MARK: - HELPERS
-  
-  func animatePresentContainerView() {
-    UIView.animate(withDuration: 0.3) {
-      self.containerViewBottomInset?.update(inset: 0)
-      self.view.layoutIfNeeded()
-    }
-  }
-  
-  func animateShowDimmedView() {
-    dimmedView.alpha = 0
-    UIView.animate(withDuration: 0.4) {
-      self.dimmedView.alpha = 0.6
-    }
-  }
-  
+
+  /// 창을 종료할 때, dismiss(animated:)가 아닌 이 메서드를 호출해야합니다.
   public func animateDismissView() {
     UIView.animate(withDuration: 0.3) {
       self.containerViewBottomInset?.update(inset: -294)
       self.view.layoutIfNeeded()
     }
-    
+
     self.dimmedView.alpha = 0.6
     UIView.animate(withDuration: 0.4) {
       self.dimmedView.alpha = 0
@@ -186,16 +203,30 @@ open class BaseBottomSheet: BaseViewController {
       self.dismiss(animated: false)
     }
   }
-  
-  func animateContainerHeight(_ height: CGFloat) {
+
+  private func animatePresentContainerView() {
+    UIView.animate(withDuration: 0.3) {
+      self.containerViewBottomInset?.update(inset: 0)
+      self.view.layoutIfNeeded()
+    }
+  }
+
+  private func animateShowDimmedView() {
+    dimmedView.alpha = 0
+    UIView.animate(withDuration: 0.4) {
+      self.dimmedView.alpha = 0.6
+    }
+  }
+
+  private func animateContainerHeight(_ height: CGFloat) {
     UIView.animate(withDuration: 0.4) {
       self.containerViewHeight?.update(offset: height)
       self.view.layoutIfNeeded()
     }
   }
-  
+
   // MARK: - Functions
-  
+
   public func updateTitle(_ title: String) {
     self.titleLabel.text = title
   }
