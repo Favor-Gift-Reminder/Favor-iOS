0e0b17a7e0a67ae5d149a7168626ff99e3155019
이창준 <nomatterjun@gmail.com>
2023-05-29 10:52:26 +0900

    [Feat] FavorBarButtonItem에 문자만 들어간 항목 추가

--- Favor/FavorKit/Sources/FavorKit/Components/FavorBarButtonItem.swift
+++ Favor/FavorKit/Sources/FavorKit/Components/FavorBarButtonItem.swift
@@ -1,6 +1,6 @@
 //
 //  FavorBarButtonItem.swift
-//  
+//  Favor
 //
 //  Created by 이창준 on 2023/03/12.
 //
@@ -13,6 +13,12 @@ import SnapKit
 
 public class FavorBarButtonItem: UIBarButtonItem {
 
+  // MARK: - Constants
+
+  private enum Metric {
+    static let size: CGFloat = 44.0
+  }
+
   // MARK: - UI Components
 
   fileprivate lazy var button = UIButton()
@@ -24,10 +30,19 @@ public class FavorBarButtonItem: UIBarButtonItem {
   }
 
   /// - Parameters:
-  ///   - imageName: 아이콘 이미지 애셋의 이름
+  ///   - icon: 아이콘 이미지
   public convenience init(_ icon: UIImage.FavorIcon) {
     self.init()
     self.button = self.makeButton(with: icon)
+
+    self.setupStyles()
+    self.setupLayouts()
+    self.setupConstraints()
+  }
+
+  public convenience init(_ title: String) {
+    self.init()
+    self.button = self.makeButton(with: title)
 
     self.setupStyles()
     self.setupLayouts()
@@ -42,9 +57,7 @@ public class FavorBarButtonItem: UIBarButtonItem {
 // MARK: - UI Setup
 
 extension FavorBarButtonItem: BaseView {
-  public func setupStyles() {
-    //
-  }
+  public func setupStyles() { }
 
   public func setupLayouts() {
     self.customView = self.button
@@ -52,7 +65,7 @@ extension FavorBarButtonItem: BaseView {
 
   public func setupConstraints() {
     self.customView?.snp.makeConstraints { make in
-      make.width.height.equalTo(44)
+      make.width.height.equalTo(Metric.size)
     }
   }
 }
@@ -65,6 +78,24 @@ private extension FavorBarButtonItem {
     config.image = .favorIcon(icon)
     config.contentInsets = NSDirectionalEdgeInsets(top: 11, leading: 11, bottom: 11, trailing: 11)
     let button = UIButton(configuration: config)
+    return button
+  }
+
+  func makeButton(with title: String) -> UIButton {
+    var config = UIButton.Configuration.plain()
+    config.updateAttributedTitle(title, font: .favorFont(.bold, size: 18))
+
+    let button = UIButton(configuration: config)
+    button.configurationUpdateHandler = { button in
+      switch button.state {
+      case .disabled:
+        config.baseForegroundColor = .favorColor(.line2)
+      case .normal:
+        config.baseForegroundColor = .favorColor(.icon)
+      default:
+        break
+      }
+    }
     return button
   }
 }
