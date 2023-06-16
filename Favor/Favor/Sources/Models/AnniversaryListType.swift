d6a6f989602247ec01f194d01079b79bd40b6066
eung7 <wannabeeung@gmail.com>
2023-06-17 00:51:00 +0900

    [Feat] AnniversaryListType Friend연관값 추가

--- Favor/Favor/Sources/Models/AnniversaryListType.swift
+++ Favor/Favor/Sources/Models/AnniversaryListType.swift
@@ -1,11 +1,11 @@
 //
 //  AnniversaryListType.swift
+//  Favor
 //
-//
-//  Created by 김응철 on 6/15/23.
+//  Created by 김응철 on 6/16/23.
 //
 
-import Foundation
+import FavorKit
 
 /// 기념일 리스트를 분기처리위한 열거형입니다.
 public enum AnniversaryListType {
@@ -16,5 +16,5 @@ public enum AnniversaryListType {
   /// 유저일 경우와 그렇지 않을 경우를 구분해야합니다.
   ///
   /// *서버에서 데이터를 받아오기 전에 UI를 변경해야합니다.
-  case friend(friendUserNo: Int, isUser: Bool)
+  case friend(friend: Friend)
 }
