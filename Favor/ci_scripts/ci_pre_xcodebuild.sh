03bf6c7cae9f438098113ef66ac4260be0fbd640
Junnos  <junnos.dev@gmail.com>
2024-01-31 23:54:17 +0900

    [Fix] API-Info.plist 파일을 ignore 목록에서 제거

--- Favor/ci_scripts/ci_pre_xcodebuild.sh
+++ Favor/ci_scripts/ci_pre_xcodebuild.sh
@@ -4,6 +4,7 @@ echo "Xcode Cloud 빌드를 위한 환경을 세팅합니다..."
 
 cd ..
 
+plutil -replace INSTAGRAM_APP_ID -string $INSTAGRAM_APP_ID "FavorNetworkKit/Sources/FavorNetworkKit/API-Info.plist"
 plutil -replace API_BASEURL -string $API_BASEURL "FavorNetworkKit/Sources/FavorNetworkKit/API-Info.plist"
 
 exit 0
